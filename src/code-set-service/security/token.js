dacb08fe15984b3475d4daea76f1da70e583554b
Moad, Karl <karl.moad@gmail.com>
2016-09-12 15:12:59 -0400

    Added JWT based verification and auth context

--- src/code-set-service/security/token.js
+++ src/code-set-service/security/token.js
@@ -1,4 +1,5 @@
 'use strict';
+const jwt = require('jsonwebtoken');
 /**
  * Authorize function for securityDefinitions:token
  * type : apiKey
@@ -8,13 +9,18 @@ module.exports = function authorize(req, res, next) {
     //The context('this') for authorize will be bound to the 'securityDefinition'
     //this.name - The name of the header or query parameter to be used for securityDefinitions:token apiKey security scheme.
     //this.in - The location of the API key ("query" or "header") for securityDefinitions:token apiKey security scheme.
-    validate(req, function (error, availablescopes) {
-        var auth = req.header(this.name);
+    var auth = req.header(this.name);
+    var key = process.env.JWT_KEY;
 
-        if (auth) {
-            console.log(auth);
-            next();
-        }
-
-    });
+    if(auth) {
+        jwt.verify(auth, key, {algorithm: 'HS512'}, function (err, decoded) {
+            if (err) {
+                res.status(400).send(err);
+            }
+            else {
+                req.authenticated = decoded;
+                next();
+            }
+        });
+    }
 };
