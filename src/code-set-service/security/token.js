1d6803503bbe9b3c1a5f9bfe28dca631e20fda35
Moad, Karl <karl.moad@gmail.com>
2016-09-22 14:29:35 -0400

    Updated to retool security following Swaggerize-Express issue resolution

--- src/code-set-service/security/token.js
+++ src/code-set-service/security/token.js
@@ -10,7 +10,7 @@ module.exports = function authorize(req, res, next) {
     // The context('this') for authorize will be bound to the 'securityDefinition'
     // this.name - The name of the header or query parameter to be used for securityDefinitions:token apiKey security scheme.
     // this.in - The location of the API key ("query" or "header") for securityDefinitions:token apiKey security scheme.
-    var auth = req.header(this.name);
+    var auth = req.header['auth'];
     var key = process.env.JWT_KEY;
 
     if(auth) {
