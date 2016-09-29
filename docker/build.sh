f583455a0ee5df7c50cd781be873e15df395a212
Moad, Karl <karl.moad@gmail.com>
2016-09-29 14:02:52 -0400

    gulp docker build process

--- docker/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf app
-mkdir app
-rsync -r --exclude 'node_modules' --exclude '*.log' --exclude '*.md' --exclude 'tests' ../src/code-set-service/ ./app
-docker build -t upmc-isd-eti/esb-code-set-api .
--- docker/debug_build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf debug
-mkdir debug
-rsync -r --exclude 'node_modules' --exclude '*.log' --exclude '*.md' --exclude 'tests' ../src/code-set-service/ ./debug
-docker build -f DebugDockerfile -t upmc-isd-eti/esb-code-set-api-debug .
