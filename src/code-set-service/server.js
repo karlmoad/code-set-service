c59cf73b22bcaf48a6257a12b1b197d4bcacb37d
Moad, Karl <karl.moad@gmail.com>
2016-10-25 10:33:20 -0400

    Added CORS support

--- src/code-set-service/server.js
+++ src/code-set-service/server.js
@@ -1,14 +1,19 @@
 'use strict';
 
 var Http = require('http');
+var cors = require('cors');
 var Express = require('express');
 var BodyParser = require('body-parser');
 var Swaggerize = require('swaggerize-express');
 var Path = require('path');
+var port = process.env.PORT || 8000;
 
 var App = Express();
 
 var Server = Http.createServer(App);
+
+App.use(cors());
+App.options("*",cors());
 
 App.use(BodyParser.json());
 App.use(BodyParser.urlencoded({
@@ -22,7 +27,7 @@ App.use(Swaggerize({
     security: Path.resolve('./security')
 }));
 
-Server.listen(8000, function () {
+Server.listen(port, function () {
     App.swagger.api.host = this.address().address + ':' + this.address().port;
     /* eslint-disable no-console */
     console.log('App running on %s:%d', this.address().address, this.address().port);
